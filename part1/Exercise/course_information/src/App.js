f94098a7892cf3e1ae834ba023aea3223e334c78
mauricefh <filiatreaultmaurice@gmail.com>
2023-03-16 19:22:50 -0400

    Fix the missing export declaration at the bottom of App.js

--- part1/Exercise/course_information/src/App.js
+++ part1/Exercise/course_information/src/App.js
@@ -1,73 +1,22 @@
-const Header = props => {
-  console.log(props);
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
-
-const Content = props => {
-  console.log(props);
-  return (
-    <div>
-      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  );
-};
-
-const Total = props => {
-  console.log(props);
-  return (
-    <div>
-      <h3>Total number of exercise: {props.total}</h3>
-    </div>
-  );
-};
-
-const Part = props => {
-  console.log(props);
+const Hello = props => {
   return (
     <div>
       <p>
-        <strong>{props.parts}</strong>
+        Hello {props.name}, you are {props.age} years old
       </p>
-      <p>number of exercise: {props.exercises}</p>
     </div>
   );
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  const total =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
+  const name = "Peter";
+  const age = 10;
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
